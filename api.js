88c02fca5a96da117ec9034e1feace2a52a6fd63
new_GitHub <devanshudutta22@gamil.com>
2024-11-13 19:33:21 +0530

    vercell

--- api.js
+++ api.js
@@ -132,7 +132,8 @@ app.put('/addtocart', auth, async (req, res) => {
         }
         else
         {
-            user.cart.push({productId, title, price,image,itemquantity});
+            const quantity = itemquantity;
+            user.cart.push({productId, title, price,image,quantity});
             res.status(200).json({ message: 'Item added to cart', cart: user.cart });
             await user.save();
         }
